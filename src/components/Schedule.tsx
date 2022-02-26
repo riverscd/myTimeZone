41010293208a3f4471d065502ac0368264ea3f29
CD Rivers <riverscd90@gmail.com>
2022-02-26 16:07:10 -0500

    formatted code a bit, still room for improvement but a bit cleaner

--- src/components/Schedule.tsx
+++ src/components/Schedule.tsx
@@ -11,13 +11,11 @@ export function Schedule() {
     const [realFeelTime, setRealFeelTime] = useState(parseInt(""))
 
     function convertTime() {
-
         setTimeDifference(parseInt(simulatedWakeTime) - parseInt(currentWakeTime))
     }
     function getRealFeel() {
         setRealFeelTime(parseInt(currentTime) + timeDifference)
     }
-
     function adjustRealFeel() {
         if (realFeelTime >= 24) {
             setRealFeelTime(realFeelTime - 24)
@@ -26,45 +24,35 @@ export function Schedule() {
         }
     }
 
-
     return (
         <div>
             <div className="home-page-title">
-            <span className="material-icons-outlined">wb_sunny </span>
-            <h1>Schedule</h1>
-            <span className="material-icons-outlined">dark_mode</span>
+                <span className="material-icons-outlined">wb_sunny </span>
+                <h1>Schedule</h1>
+                <span className="material-icons-outlined">dark_mode</span>
             </div>
             <h2>Instructions</h2>
             <h3>- Today's Wake Up: Enter the time you woke up today</h3>
             <h3>- Simulated Wake Up: Enter the time you would like to feel like you woke up </h3>
             <p>sample: set to 6am to simulate a 6am wakeup
-                <br /> (set default options once figure out moment.js)
-            </p>
-
+                <br /> (set default options once figure out moment.js)</p>
             <h2>Enter Times</h2>
             <div className="current-schedule-inputs">
                 <label htmlFor="wake-up">Today's Wake Up</label>
                 <input id="wake-up" type="time" onChange={(e) => { setCurrentWakeTime(e.target.value) }} />
                 {/*<label htmlFor="bed-time">Current Bed Time</label>
-                <input id="bed-time" type="time" />
-                */}
+                <input id="bed-time" type="time" />*/}
                 <label htmlFor="simulate-wake-up" >Simulated Wake Up</label>
                 <input id="simulate-wake-up" type="time" onChange={(e) => { setSimulatedWakeTime(e.target.value) }} />
-
                 <label htmlFor="current-time">Current Time</label>
                 <input id="current-time" type="time" onChange={(e) => { setCurrentTime(e.target.value) }} />
             </div>
-
             <button onClick={(e) => { convertTime() }}>Enter</button>
-
-
-
             <h3>Values for tracking while building</h3>
             <p> today's wakeup: {currentWakeTime}</p>
             <p> simulated wakeup: {simulatedWakeTime}</p>
             <p> time difference: {timeDifference}</p>
             <p>(eventually consolidate buttons)</p>
-
             <button onClick={(e) => { getRealFeel() }}>Translate</button>
             <h2>Translation Results</h2>
             <p> current time: {currentTime}</p>
